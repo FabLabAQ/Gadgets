d8dd142b742d70b6514f2c7de4d912178d8d50d9
Luca Anastasio <anastasio.lu@gmail.com>
2016-08-17 19:20:56 +0200

    sketck v0.2

--- ATtiny LED badge/ATtinyLEDbadge02/ATtinyLEDbadge02.ino
+++ ATtiny LED badge/ATtinyLEDbadge02/ATtinyLEDbadge02.ino
@@ -27,7 +27,7 @@
 #define char_Y 0b10011001011000101100
 #define char_Z 0b11110001001001001111
 #define char_space 0b00000000000000000000
-#define char_\! 0b01000100010000000100
+// #define char_! 0b01000100010000000100
 
 // where does our characterMap start in the ASCII code
 #define MAP_START      32
@@ -41,14 +41,18 @@
 // the text to display
 #define DISPLAY_STRING "HELLO WORLD  "
 
+#define STRING_LENGTH 9
+
+const unsigned long PROGMEM display_string[STRING_LENGTH] = {char_F, char_A, char_B, char_L, char_A, char_B, char_A, char_Q};
+
 
 // maps characters to their 4x5 grid 
-unsigned long characterMap[59];
+//unsigned long characterMap[59];
 
 // set up a character in the characterMap
-void Chr(char theChar, unsigned long value) {
-  characterMap[theChar - MAP_START] = value;
-}
+//void Chr(char theChar, unsigned long value) {
+//  characterMap[theChar - MAP_START] = value;
+//}
 
 // The offset of our string in the display
 int offset = 0;
@@ -56,26 +60,41 @@ unsigned long lastMillis = 0;
 unsigned long currentMillis = 0;
 unsigned int timeout;
 
-char myString[] = DISPLAY_STRING;
-int length = sizeof(myString);
+// char myString[] = DISPLAY_STRING;
+int length = STRING_LENGTH;
 
 // render the string on the given offset
-void renderString(char *theString, int offset) {
+void renderString(int offset) {
   int index = 0;
-  while (theString[index]) {
-    renderCharacter(theString[index], offset - index * (DISPLAY_WIDTH + 1));
+  while (display_string[index]) {
+    renderCharacter(display_string[index], offset - index * (DISPLAY_WIDTH + 1));
     index++;
   }
 }
 
 // render a character on the given offset
-void renderCharacter(char theChar, int charOffset) {
+//void renderCharacter(char theChar, int charOffset) {
+//  if (charOffset <= -DISPLAY_WIDTH || charOffset > DISPLAY_WIDTH) {
+//    // off the 'screen' nothing to do
+//    return;
+//  }
+//
+//  unsigned long graphic = characterMap[theChar - MAP_START];
+//
+//  for (byte y = 0; y < DISPLAY_HEIGHT; y++) {
+//    for (byte x = 0; x < DISPLAY_WIDTH; x++) {
+//      // 3 - x to reverse order
+//      setPixel(3 - x - charOffset, y, graphic & 0x1);
+//      graphic = graphic >> 1;
+//    }
+//  }
+//}
+
+void renderCharacter(unsigned long graphic, int charOffset) {
   if (charOffset <= -DISPLAY_WIDTH || charOffset > DISPLAY_WIDTH) {
     // off the 'screen' nothing to do
     return;
   }
-
-  unsigned long graphic = characterMap[theChar - MAP_START];
 
   for (byte y = 0; y < DISPLAY_HEIGHT; y++) {
     for (byte x = 0; x < DISPLAY_WIDTH; x++) {
@@ -99,11 +118,14 @@ void setPixel(byte x, byte y, boolean ledStatus) {
 // turn on the pins to light a LED
 void setLed(byte vin, byte gnd, boolean ledStatus) {
   delay(1);
-  pinMode(0, INPUT); 
-  pinMode(1, INPUT); 
-  pinMode(2, INPUT); 
-  pinMode(3, INPUT); 
-  pinMode(4, INPUT); 
+//  pinMode(0, INPUT); 
+//  pinMode(1, INPUT); 
+//  pinMode(2, INPUT); 
+//  pinMode(3, INPUT); 
+//  pinMode(4, INPUT);
+  for(uint8_t i=0; i<=4; i++) {
+    pinMode(i, INPUT);
+  }
 
   if(!ledStatus) return;
 
@@ -117,35 +139,35 @@ void setLed(byte vin, byte gnd, boolean ledStatus) {
 void setup() {
   // set up render map
 
-  // Rows:   1---2---3---4---5---
-  Chr('A', 0b01101001111110011001);
-  Chr('B', 0b11101001111010011110);
-  Chr('C', 0b01111000100010000111);
-  Chr('D', 0b11101001100110011110);
-  Chr('E', 0b11111000111010001111);
-  Chr('F', 0b11111000111010001000);
-  Chr('G', 0b01111000101110010110);
-  Chr('H', 0b10011001111110011001);
-  Chr('I', 0b01110010001000100111);
-  Chr('J', 0b01110010001010100100);
-  Chr('K', 0b10011010110010101001);
-  Chr('L', 0b10001000100010001111);
-  Chr('M', 0b10011111111110011001);
-  Chr('N', 0b10011101101110011001);
-  Chr('O', 0b01101001100110010110);
-  Chr('P', 0b11101001111010001000);
-  Chr('Q', 0b01101001101101100001);
-  Chr('R', 0b11101001111010101001);
-  Chr('S', 0b11111000111100011111);
-  Chr('T', 0b01110010001000100010);
-  Chr('U', 0b10011001100110010110);
-  Chr('V', 0b10011001100110100100);
-  Chr('W', 0b10011001111111110110);
-  Chr('X', 0b10011001011010011001);
-  Chr('Y', 0b10011001011000101100);
-  Chr('Z', 0b11110001001001001111);
-  Chr(' ', 0b00000000000000000000);
-  Chr('!', 0b01000100010000000100);
+//  // Rows:   1---2---3---4---5---
+//  Chr('A', 0b01101001111110011001);
+//  Chr('B', 0b11101001111010011110);
+//  Chr('C', 0b01111000100010000111);
+//  Chr('D', 0b11101001100110011110);
+//  Chr('E', 0b11111000111010001111);
+//  Chr('F', 0b11111000111010001000);
+//  Chr('G', 0b01111000101110010110);
+//  Chr('H', 0b10011001111110011001);
+//  Chr('I', 0b01110010001000100111);
+//  Chr('J', 0b01110010001010100100);
+//  Chr('K', 0b10011010110010101001);
+//  Chr('L', 0b10001000100010001111);
+//  Chr('M', 0b10011111111110011001);
+//  Chr('N', 0b10011101101110011001);
+//  Chr('O', 0b01101001100110010110);
+//  Chr('P', 0b11101001111010001000);
+//  Chr('Q', 0b01101001101101100001);
+//  Chr('R', 0b11101001111010101001);
+//  Chr('S', 0b11111000111100011111);
+//  Chr('T', 0b01110010001000100010);
+//  Chr('U', 0b10011001100110010110);
+//  Chr('V', 0b10011001100110100100);
+//  Chr('W', 0b10011001111111110110);
+//  Chr('X', 0b10011001011010011001);
+//  Chr('Y', 0b10011001011000101100);
+//  Chr('Z', 0b11110001001001001111);
+//  Chr(' ', 0b00000000000000000000);
+//  Chr('!', 0b01000100010000000100);
 
   // how long to wait between shifting the display
   timeout = 1000 / SPEED;
@@ -155,7 +177,7 @@ void setup() {
 void loop() {
   currentMillis = millis();
 
-  renderString(myString, offset);
+  renderString(offset);
 
   if (currentMillis - lastMillis > timeout) {
     lastMillis = currentMillis;
