f48058b7399cba7a928b249b33d37cec41051c55
Luca Anastasio <anastasio.lu@gmail.com>
2016-11-12 18:25:08 +0100

    new sketch

---
+++ ATtiny LED badge/ATtinyLEDbadge_0.3/charactermap.h
@@ -0,0 +1,29 @@
+// Character set
+// Rows:         1---2---3---4---5---
+const unsigned long  char_A = 0b01101001111110011001; // dec 65
+const unsigned long  char_B = 0b11101001111010011110;
+const unsigned long  char_C = 0b01111000100010000111;
+const unsigned long  char_D = 0b11101001100110011110;
+const unsigned long  char_E = 0b11111000111010001111;
+const unsigned long  char_F = 0b11111000111010001000;
+const unsigned long  char_G = 0b01111000101110010110;
+const unsigned long  char_H = 0b10011001111110011001;
+const unsigned long  char_I = 0b01110010001000100111;
+const unsigned long  char_J = 0b01110010001010100100;
+const unsigned long  char_K = 0b10011010110010101001;
+const unsigned long  char_L = 0b10001000100010001111;
+const unsigned long  char_M = 0b10011111111110011001;
+const unsigned long  char_N = 0b10011101101110011001;
+const unsigned long  char_O = 0b01101001100110010110;
+const unsigned long  char_P = 0b11101001111010001000;
+const unsigned long  char_Q = 0b01101001101101100001;
+const unsigned long  char_R = 0b11101001111010101001;
+const unsigned long  char_S = 0b11111000111100011111;
+const unsigned long  char_T = 0b01110010001000100010;
+const unsigned long  char_U = 0b10011001100110010110;
+const unsigned long  char_V = 0b10011001100110100100;
+const unsigned long  char_W = 0b10011001111111110110;
+const unsigned long  char_X = 0b10011001011010011001;
+const unsigned long  char_Y = 0b10011001011000101100;
+const unsigned long  char_Z = 0b11110001001001001111; // dec 90
+const unsigned long  char_space = 0b00000000000000000000; // dec 32
